6487d07318322fda46084bbee0923742f8d56bde
Adrian Renner <addyi@users.noreply.github.com>
2023-12-04 07:20:23 +0100

    Day 04 Part 2

--- src/day04/Day04.kt
+++ src/day04/Day04.kt
@@ -17,15 +17,20 @@ fun main() {
                     val winningNumbers = game[1]
 
 
-                    myNumbers.extractNumbers().intersect(winningNumbers.extractNumbers()).also { it.println() }
+                    myNumbers.extractNumbers().intersect(winningNumbers.extractNumbers())
                 }
                 .let {
                     2.toDouble().pow(it.size.toDouble() - 1).toInt()
                 }
-                .also { it.println() }
         }
 
 
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.mapIndexed { index, _ ->
+            numOfWinningCards(input, index)
+        }.sum()
     }
 
     val testInput = readInput("day04/Day04_test")
@@ -34,12 +39,45 @@ fun main() {
         check(it == 13)
     }
 
+    part2(testInput).also {
+        it.println()
+        check(it == 30)
+    }
+
     val input = readInput("day04/Day04")
     part1(input).println()
-    //part2(input).println()
+    part2(input).println()
 }
 
 private fun String.extractNumbers(): List<Int> = this
     .split(" ")
     .filter { it.isNotBlank() && it.first().isDigit() }
     .map { it.toInt() }
+
+private fun numOfWinningCards(input: List<String>, cardNum: Int): Int {
+    //cardNum.println()
+
+    if (cardNum >= input.size) return 0
+
+    var res = 1
+    val numberOfMatching = input[cardNum].numberOfMatching()
+
+    for (i in 1..numberOfMatching) {
+        res += numOfWinningCards(input, cardNum + i)
+    }
+
+    return res
+}
+
+
+private fun String.numberOfMatching(): Int = this
+    .split(":")[1]
+    .split("|")
+    .let { game ->
+        check(game.size == 2)
+        val myNumbers = game.first()
+        val winningNumbers = game[1]
+
+        myNumbers.extractNumbers().intersect(winningNumbers.extractNumbers())
+    }
+    .size
