9e18b3940051178351d2aa4eaa8a1f02cc0aaa8a
Adrian Renner <addyi@users.noreply.github.com>
2023-12-05 09:18:10 +0100

    Day 5 Part 2

--- src/day05/Day05.kt
+++ src/day05/Day05.kt
@@ -34,16 +34,66 @@ fun main() {
     }
 
 
+    fun part2(input: List<String>): Long {
+        val seeds = input[0]
+            .dropWhile { !it.isDigit() }
+            .split(" ")
+            .map { it.toLong() }
+            .windowed(2, 2)
+            .map { LongRange(it[0], (it[0] + it[1]) - 1) }
+            .sortedBy { it.first }
+            .mergeOverlappingRanges()
+
+        val seedToSoilMap = input.getMapWithName("seed-to-soil map:").getRangeMapping()
+        val soilToFertilizerMap = input.getMapWithName("soil-to-fertilizer map:").getRangeMapping()
+        val fertilizerToWaterMap = input.getMapWithName("fertilizer-to-water map:").getRangeMapping()
+        val waterToLightMap = input.getMapWithName("water-to-light map:").getRangeMapping()
+        val lightToTemperatureMap = input.getMapWithName("light-to-temperature map:").getRangeMapping()
+        val temperatureToHumidityMap = input.getMapWithName("temperature-to-humidity map:").getRangeMapping()
+        val humidityToLocationMap = input.getMapWithName("humidity-to-location map:").getRangeMapping()
+
+        return seeds
+            .map { seedRanges ->
+                seedRanges.minOf {
+                    seedToSoilMap.getMapped(it).let { soil ->
+                        soilToFertilizerMap.getMapped(soil).let { fertilizer ->
+                            fertilizerToWaterMap.getMapped(fertilizer).let { water ->
+                                waterToLightMap.getMapped(water).let { light ->
+                                    lightToTemperatureMap.getMapped(light).let { temperature ->
+                                        temperatureToHumidityMap.getMapped(temperature).let { humidity ->
+                                            humidityToLocationMap.getMapped(humidity)
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            .min()
+    }
+
     val testInput = readInput("day05/Day05_test")
     part1(testInput).also {
         it.println()
         check(it == 35L)
     }
 
+    part2(testInput).also {
+        it.println()
+        check(it == 46L)
+    }
 
     val input = readInput("day05/Day05")
-    part1(input).println()
-    // part2(input).println()
+    part1(input).also {
+        it.println()
+        check(it == 1181555926L)
+    }
+
+    part2(input).also {
+        it.println()
+        check(it == 37806486L)
+    }
 }
 
 private fun List<RangeMapping>.getMapped(id: Long): Long = this
@@ -72,3 +122,26 @@ private fun List<String>.getMapWithName(name: String): List<String> = this
     .dropWhile { it != name }
     .drop(1)
     .takeWhile { it.isNotBlank() && it[0].isDigit() }
+
+private fun List<LongRange>.mergeOverlappingRanges(): List<LongRange> {
+    val reducedSetOfRanges = mutableListOf<LongRange>()
+
+    var currentRange = this[0]
+
+    for (i in 1 until this.size) {
+        val nextRange = this[i]
+
+        currentRange = if (currentRange.last >= nextRange.first) {
+            // Merge the ranges
+            currentRange.first..maxOf(currentRange.last, nextRange.last)
+        } else {
+            // Add the current range to the result and move to the next one
+            reducedSetOfRanges.add(currentRange)
+            nextRange
+        }
+    }
+
+    // Add the last range
+    reducedSetOfRanges.add(currentRange)
+    return reducedSetOfRanges
+}
